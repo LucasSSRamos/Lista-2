260f9695b84d7d3209a55f73bab30f7947475ab3
LucasSSRamos <110167908+LucasSSRamos@users.noreply.github.com>
2023-09-04 23:54:41 -0300

    Update AlunoViewModel.cs

--- Models/Request/AlunoViewModel.cs
+++ Models/Request/AlunoViewModel.cs
@@ -1,10 +1,14 @@
-﻿namespace Aluno.Models.Request
+﻿using Aluno.Models.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aluno.Models.Request
 {
     public class AlunoViewModel
     {
-
+        [Required(ErrorMessage = "O nome é obrigatorio!! ")]
         public string Nome { get; set; }
 
+        [RaValidation]
         public string RA { get; set; }
 
         public string Email { get; set; }
