1ae13a3cfd800cb8c8e43592d0a6688cf8a9e23b
LucasSSRamos <110167908+LucasSSRamos@users.noreply.github.com>
2023-09-05 00:03:45 -0300

    Update ApiResponse.cs

--- Configuration/ApiResponse.cs
+++ Configuration/ApiResponse.cs
@@ -1,1 +1,12 @@
+﻿namespace Aluno.Configuration
+{
+    public class ApiResponse<T>
+    {
+        public bool Sucess { get; set; }
 
+        public T Data { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
